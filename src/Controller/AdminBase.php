1d784228022ac36c578c251aeaa43e6cfeeb7b1e
haomvc <haomvc001@qq.com>
2021-12-02 09:26:36 +0800

    001

---
+++ src/Controller/AdminBase.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | Created by Laotian on 2021-07-30.
+// | Copyright 2021 HaoMVC. All rights reserved.
+// +----------------------------------------------------------------------
+
+namespace HaoMVC\Controller;
+
+
+/**
+ * 
+ */
+class AdminBase extends Base
+{
+	
+	public function Index()
+	{
+		return ''
+	}
+}
+
